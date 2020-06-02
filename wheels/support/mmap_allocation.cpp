88ba2ccbc867ad852985a3f2f3ab3fe24a818fd4
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-06-02 10:41:34 +0300

    wheels

---
+++ wheels/support/mmap_allocation.cpp
@@ -0,0 +1,68 @@
+#include <twist/memory/mmap_allocation.hpp>
+
+#include <twist/support/assert.hpp>
+
+#include <sys/mman.h>
+
+namespace twist {
+
+#define CHECK_RESULT(ret, error) TWIST_VERIFY(ret != -1, error)
+
+//////////////////////////////////////////////////////////////////////
+
+static size_t PagesToBytes(size_t count) {
+  static const size_t kPageSize = 4096;
+
+  return count * kPageSize;
+}
+
+MmapAllocation MmapAllocation::AllocatePages(size_t count) {
+  size_t size = PagesToBytes(count);
+
+  void* start = mmap(/*addr=*/nullptr, /*length=*/size,
+                     /*prot=*/PROT_READ | PROT_WRITE,
+                     /*flags=*/MAP_PRIVATE | MAP_ANONYMOUS,
+                     /*fd=*/-1, /*offset=*/0);
+
+  TWIST_VERIFY(start != MAP_FAILED, "Cannot allocate " << count << " pages");
+
+  return MmapAllocation{(char*)start, size};
+}
+
+void MmapAllocation::ProtectPages(size_t offset, size_t count) {
+  int ret = mprotect(/*addr=*/(void*)(start_ + PagesToBytes(offset)),
+                     /*len=*/PagesToBytes(count),
+                     /*prot=*/PROT_NONE);
+  CHECK_RESULT(
+      ret, "Cannot protect pages [" << offset << ", " << offset + count << ")");
+}
+
+MmapAllocation::MmapAllocation(MmapAllocation&& that) {
+  start_ = that.start_;
+  size_ = that.size_;
+  that.Reset();
+}
+
+MmapAllocation& MmapAllocation::operator=(MmapAllocation&& that) {
+  Release();
+  start_ = that.start_;
+  size_ = that.size_;
+  that.Reset();
+  return *this;
+}
+
+void MmapAllocation::Release() {
+  if (start_ == nullptr) {
+    return;
+  }
+
+  int ret = munmap((void*)start_, size_);
+  CHECK_RESULT(ret, "Cannot unmap allocated pages");
+}
+
+void MmapAllocation::Reset() {
+  start_ = nullptr;
+  size_ = 0;
+}
+
+}  // namespace twist
---
+++ wheels/support/mmap_allocation.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <twist/memory/memspan.hpp>
+
+namespace twist {
+
+class MmapAllocation {
+ public:
+  MmapAllocation() {
+    Reset();
+  }
+
+  // Allocate `count` pages of zeroed memory
+  static MmapAllocation AllocatePages(size_t count);
+
+  // Moving
+  MmapAllocation(MmapAllocation&& that);
+  MmapAllocation& operator=(MmapAllocation&& that);
+
+  ~MmapAllocation() {
+    Release();
+  }
+
+  char* Start() const {
+    return start_;
+  }
+
+  char* End() const {
+    return start_ + size_;
+  }
+
+  size_t Size() const {
+    return size_;
+  }
+
+  MemSpan AsMemSpan() const {
+    return MemSpan(start_, size_);
+  }
+
+  // Protect range of pages
+  // Protected pages cannot be read, written or executed
+  void ProtectPages(size_t offset, size_t count);
+
+  void Release();
+
+ private:
+  MmapAllocation(char* start, size_t size) : start_(start), size_(size) {
+  }
+
+  void Reset();
+
+ private:
+  // Aligned to page boundary
+  char* start_;
+  size_t size_;
+};
+
+}  // namespace twist
