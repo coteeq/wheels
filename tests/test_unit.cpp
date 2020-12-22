acbbdb2c214e76a8728cba102ac3224b661bbad6
Roman Lipovsky <roman.lipovsky@gmail.com>
2020-12-22 13:12:00 +0300

    better Unit

---
+++ tests/test_unit.cpp
@@ -0,0 +1,18 @@
+#include <wheels/support/unit.hpp>
+
+#include <wheels/test/test_framework.hpp>
+
+#include <vector>
+
+TEST_SUITE(Unit) {
+  SIMPLE_TEST(Container) {
+    std::vector<wheels::Unit> units;
+  }
+
+  SIMPLE_TEST(Compare) {
+    wheels::Unit u1;
+    wheels::Unit u2;
+    ASSERT_TRUE(u1 == u2);
+    ASSERT_FALSE(u1 != u2);
+  }
+}
--- wheels/support/unit.hpp
+++ wheels/support/unit.hpp
@@ -2,6 +2,15 @@
 
 namespace wheels {
 
+// https://en.wikipedia.org/wiki/Unit_type
+
 struct Unit {};
+
+inline bool operator==(const Unit& /*lhs*/, const Unit& /*rhs*/) {
+  return true;
+}
+inline bool operator!=(const Unit& /*lhs*/, const Unit& /*rhs*/) {
+  return false;
+}
 
 }  // namespace wheels
