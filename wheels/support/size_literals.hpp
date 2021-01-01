9af2d7e5d454c3a0ff8f291f37c3a9465a85c9e5
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-01-01 22:27:00 +0200

    better size literals

--- wheels/support/size_literals.hpp
+++ wheels/support/size_literals.hpp
@@ -11,11 +11,11 @@ size_t operator""_KiB(TCount count) {
 }
 
 size_t operator""_MiB(TCount count) {
-  return count * 1024 * 1024;
+  return count * 1024_KiB;
 }
 
 size_t operator""_GiB(TCount count) {
-  return (size_t)count * 1024 * 1024 * 1024;
+  return (size_t)count * 1024_MiB;
 }
 
 }  // namespace wheels::size_literals
