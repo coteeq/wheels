5527c40203ac4753d927e2fffc05cfbfd0098d7e
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-01-25 20:09:56 +0700

    HashCombine

---
+++ wheels/support/hash_combine.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstdlib>
+
+namespace wheels {
+
+void HashCombine(size_t& digest, size_t hash_value) {
+  // https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine
+  digest ^= (hash_value + 0x9e3779b9 + (digest << 6) + (digest >> 2));
+}
+
+}  // namespace wheels
