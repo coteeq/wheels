c657ae12fcaacb13e7015ee82a8168e8303a0d6f
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-01-29 02:39:03 +0700

    fix hash combine

--- wheels/support/hash_combine.hpp
+++ wheels/support/hash_combine.hpp
@@ -4,7 +4,7 @@
 
 namespace wheels {
 
-void HashCombine(size_t& digest, size_t hash_value) {
+inline void HashCombine(size_t& digest, size_t hash_value) {
   // https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine
   digest ^= (hash_value + 0x9e3779b9 + (digest << 6) + (digest >> 2));
 }
