f29ad064aeff1b2549fde40b619f844626cb5be4
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-01-28 14:32:20 +0700

    Unit via std::monostate

--- wheels/support/unit.hpp
+++ wheels/support/unit.hpp
@@ -1,17 +1,12 @@
 #pragma once
 
+#include <variant>
+
 namespace wheels {
 
 // https://en.wikipedia.org/wiki/Unit_type
 // https://github.com/facebook/folly/blob/master/folly/Unit.h
 
-struct Unit {};
-
-inline bool operator==(const Unit& /*lhs*/, const Unit& /*rhs*/) {
-  return true;
-}
-inline bool operator!=(const Unit& /*lhs*/, const Unit& /*rhs*/) {
-  return false;
-}
+using Unit = std::monostate;
 
 }  // namespace wheels
