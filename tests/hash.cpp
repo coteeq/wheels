4e48e5237c819da0138aadb3ec0626172a090ec3
Roman Lipovsky <roman.lipovsky@gmail.com>
2021-08-17 13:32:01 +0300

    HashRange

---
+++ tests/hash.cpp
@@ -0,0 +1,25 @@
+#include <wheels/support/hash.hpp>
+#include <wheels/support/view_util.hpp>
+
+#include <wheels/test/test_framework.hpp>
+
+#include <iostream>
+
+using wheels::ViewOf;
+using wheels::HashRange;
+
+TEST_SUITE(Hash) {
+  SIMPLE_TEST(HashRange) {
+    static const char* kMessage1 = "Hello, world!";
+    static const char* kMessage2 = "Hell0, world!";
+
+    size_t h1 = HashRange(13, ViewOf(kMessage1));
+    size_t h2 = HashRange(13, ViewOf(kMessage2));
+    size_t h3 = HashRange(13, ViewOf(kMessage1));
+    size_t h4 = HashRange(12, ViewOf(kMessage2));
+
+    ASSERT_EQ(h1, h3);
+    ASSERT_NE(h1, h2);
+    ASSERT_NE(h2, h4);
+  }
+}
--- wheels/support/hash.hpp
+++ wheels/support/hash.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <wheels/support/memspan.hpp>
+
 #include <cstdlib>
 
 namespace wheels {
@@ -7,6 +9,14 @@ namespace wheels {
 inline void HashCombine(size_t& digest, size_t hash_value) {
   // https://stackoverflow.com/questions/4948780/magic-number-in-boosthash-combine
   digest ^= (hash_value + 0x9e3779b9 + (digest << 6) + (digest >> 2));
+}
+
+inline size_t HashRange(size_t seed, ConstMemView range) {
+  size_t digest = seed;
+  for (const char* ptr = range.Begin(); ptr != range.End(); ++ptr) {
+    HashCombine(digest, (size_t)*ptr);
+  }
+  return digest;
 }
 
 }  // namespace wheels
