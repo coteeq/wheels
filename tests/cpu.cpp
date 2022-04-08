f9405c734cdb4d2fadd472f20191b15fd599f514
Roman Lipovsky <roman.lipovsky@gmail.com>
2022-04-08 12:19:05 +0300

    SpinLockPause for arm, disable GetCpuCycleCount

--- tests/cpu.cpp
+++ tests/cpu.cpp
@@ -6,19 +6,19 @@
 #include <random>
 
 TEST_SUITE(Cpu) {
-  SIMPLE_TEST(Rdtsc) {
-    auto start = wheels::GetCpuCycleCount();
-
-    std::mt19937 twister;
-
-    size_t total = 0;
-    for (size_t i = 0; i < 1000; ++i) {
-      total += twister();
-    }
-
-    auto end = wheels::GetCpuCycleCount();
-    std::cout << "Loop cycle count: " << end - start << std::endl;
-  }
+//  SIMPLE_TEST(Rdtsc) {
+//    auto start = wheels::GetCpuCycleCount();
+//
+//    std::mt19937 twister;
+//
+//    size_t total = 0;
+//    for (size_t i = 0; i < 1000; ++i) {
+//      total += twister();
+//    }
+//
+//    auto end = wheels::GetCpuCycleCount();
+//    std::cout << "Loop cycle count: " << end - start << std::endl;
+//  }
 
   SIMPLE_TEST(SpinLockPause) {
     for (size_t i = 0; i < 1005001; ++i) {
--- wheels/support/cpu.hpp
+++ wheels/support/cpu.hpp
@@ -6,11 +6,11 @@ namespace wheels {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-inline uint64_t GetCpuCycleCount() {
-  uint32_t hi, lo;
-  __asm__ volatile("rdtsc" : "=a"(lo), "=d"(hi));
-  return (static_cast<uint64_t>(hi) << 32) | lo;
-}
+//inline uint64_t GetCpuCycleCount() {
+//  uint32_t hi, lo;
+//  __asm__ volatile("rdtsc" : "=a"(lo), "=d"(hi));
+//  return (static_cast<uint64_t>(hi) << 32) | lo;
+//}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -20,7 +20,13 @@ inline uint64_t GetCpuCycleCount() {
 // https://aloiskraus.wordpress.com/2018/06/16/why-skylakex-cpus-are-sometimes-50-slower-how-intel-has-broken-existing-code/
 
 inline void SpinLockPause() {
+#if defined(wheels_arch_x86_64)
   asm volatile("pause\n" : : : "memory");
+#elif defined(wheels_arch_armv8_a_64)
+  asm volatile("yield\n" : : : "memory");
+#else
+  ;
+#endif
 }
 
 }  // namespace wheels
