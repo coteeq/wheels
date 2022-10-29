0127d862a271afc5d6fb87699152d9af3dd43099
Roman Lipovsky <roman.lipovsky@gmail.com>
2022-10-29 14:47:57 +0300

    LeakySingleton

---
+++ tests/singleton.cpp
@@ -0,0 +1,23 @@
+#include <wheels/test/test_framework.hpp>
+
+#include <wheels/support/singleton.hpp>
+
+TEST_SUITE(Singleton) {
+  struct Box {
+    void Set(std::string data) {
+      data_ = data;
+    }
+
+    std::string Get() const {
+      return data_;
+    }
+
+   private:
+    std::string data_;
+  };
+
+  SIMPLE_TEST(Leaky) {
+    LeakySingleton<Box>().Set("Just works");
+    ASSERT_EQ(LeakySingleton<Box>().Get(), "Just works");
+  }
+}
--- wheels/support/singleton.hpp
+++ wheels/support/singleton.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+//////////////////////////////////////////////////////////////////////
+
 // Meyers singleton
 // Thread-safe since C++11
 
@@ -7,4 +9,31 @@ template <class T>
 T* Singleton() {
   static T instance;
   return &instance;
+}
+
+//////////////////////////////////////////////////////////////////////
+
+namespace detail {
+
+template <typename T>
+class LeakyStorage {
+ public:
+  LeakyStorage() {
+    new (buffer_) T();
+  }
+
+  T& GetInstance() {
+    return *reinterpret_cast<T*>(buffer_);
+  }
+
+ private:
+  alignas(T) char buffer_[sizeof(T)];
+};
+
+}  // namespace detail
+
+template <typename T>
+T& LeakySingleton() {
+  static detail::LeakyStorage<T> leaky;
+  return leaky.GetInstance();
 }
